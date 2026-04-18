4ed2de347eb25d95dcd656c8edce493fe0d488c4
m40503528-pixel <m40503528@gmail.com>
2026-04-18 16:37:07 +0300

    Delete titan_kernel.js

--- titan_kernel.js
+++
@@ -1,9 +0,0 @@
-self.onmessage = (e) => {
-    if (e.data.type === 'INIT') console.log("Kernel: Online");
-    
-    if (e.data.type === 'ENCRYPT') {
-        // Имитация шифрования Double Ratchet
-        const encrypted = `[SECURE_DATA] ${e.data.data}`;
-        self.postMessage({ type: 'CIPHER', payload: encrypted });
-    }
-};
